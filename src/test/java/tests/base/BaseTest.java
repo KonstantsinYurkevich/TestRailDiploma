af1f70332b975426c405da6003bf6d3056331090
Konstantsin Yurkevich <kutcevolov@icloud.com>
2021-06-24 21:30:51 +0300

    --headless

--- src/test/java/tests/base/BaseTest.java
+++ src/test/java/tests/base/BaseTest.java
@@ -34,7 +34,7 @@ public abstract class BaseTest {
             WebDriverManager.chromedriver().setup();
             options.addArguments("--no-sandbox");
             options.addArguments("--start-maximized");
-         /*   options.addArguments("--headless");*/
+            options.addArguments("--headless");
             driver = new ChromeDriver(options);
         } else if (browser.equals("firefox")) {
             WebDriverManager.firefoxdriver().setup();
