1b4bc532a43a5492e620232f13c28e37bb9e0597
GhostRider <kutcevolov@icloud.com>
2021-07-18 22:21:44 +0300

    Add API Tests: 1) AddProjectApiTest 2) AddSectionAndTestCaseTest

---
+++ src/test/java/tests/api/AddTestSectionAndTestCaseTest.java
@@ -0,0 +1,63 @@
+package tests.api;
+
+import adapters.BaseAdapter;
+import adapters.ProjectSection;
+import adapters.ProjectTestCase;
+import modals.ResponseStatus;
+import modals.Section;
+import modals.TestCase;
+import org.testng.annotations.Test;
+
+import static modals.UserFactory.randomString;
+import static org.testng.Assert.assertEquals;
+
+public class AddTestSectionAndTestCaseTest extends BaseAdapter {
+
+    private static final int projectId = 115;
+
+    @Test(description = "Test case section should be added to the project")
+    public void sectionShouldBeAddToTheExistingProject() {
+        Section section = Section.builder()
+                .name(randomString())
+                .build();
+        ResponseStatus responseStatusSection = new ProjectSection().create(section, 200, projectId);
+        assertEquals(responseStatusSection.getName(), section.getName());
+    }
+
+    @Test(description = "Test case section should be deleted from the project")
+    public void sectionShouldBeDeleted() {
+        Section section = Section.builder()
+                .name(randomString())
+                .build();
+        ResponseStatus responseStatusSection = new ProjectSection().create(section, 200, projectId);
+        assertEquals(responseStatusSection.getName(), section.getName());
+        ResponseStatus responseStatusSectionDelete = new ProjectSection().delete(200, responseStatusSection.getId());
+    }
+
+    @Test(description = "Test case should be added to the project")
+    public void testCaseShouldBeAddToTheProjectWithSection() {
+        Section section = Section.builder()
+                .name(randomString())
+                .build();
+        ResponseStatus responseStatusSection = new ProjectSection().create(section, 200, projectId);
+        TestCase testCase = TestCase.builder()
+                .title(randomString())
+                .build();
+        ResponseStatus responseStatusTestCease = new ProjectTestCase().create(testCase, 200, responseStatusSection.getId());
+        assertEquals(responseStatusTestCease.getTitle(), testCase.getTitle());
+    }
+
+    @Test(description = "Test case  should be deleted from the project")
+    public void testCaseShouldBeDeleted() {
+        Section section = Section.builder()
+                .name(randomString())
+                .build();
+        ResponseStatus responseStatusSection = new ProjectSection().create(section, 200, projectId);
+        TestCase testCase = TestCase.builder()
+                .title(randomString())
+                .build();
+        ResponseStatus responseStatusTestCeaseCreate = new ProjectTestCase().create(testCase, 200, responseStatusSection.getId());
+        assertEquals(responseStatusTestCeaseCreate.getTitle(), testCase.getTitle());
+        ResponseStatus responseStatusTestCeaseDelete = new ProjectTestCase().delete(200, responseStatusTestCeaseCreate.getId());
+    }
+}
